d42e8385438893891a3661df251747a170b7d8e7
eric-stephenson <rocthered168@gmail.com>
2020-02-29 15:11:37 -0800

    progress

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,4 +1,8 @@
 #include "../includes_usr/fileIO.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
@@ -7,7 +11,21 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
-	return SUCCESS;
+	ifstream myInputFile;
+	myInputFile.open(filename,ios::in);
+	string line;
+	book b = new book();
+	if (!myInputFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	while(!myInputFile.eof()) {
+
+		getline(myInputFile, line);
+		books.push_back(book().title);
+		return SUCCESS;
+	}
+	return NO_BOOKS_IN_LIBRARY;
 }
 
 /* serializes books to the file filename
